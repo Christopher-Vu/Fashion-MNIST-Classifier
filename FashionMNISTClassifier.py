15d08ef0672004fefab1a08c59a65fd8ef2c01c0
Christopher Vu <107224696+Christopher-Vu@users.noreply.github.com>
2024-05-03 09:33:09 -0700

    Update FashionMNISTClassifier.py

--- FashionMNISTClassifier.py
+++ FashionMNISTClassifier.py
@@ -10,9 +10,6 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
 import matplotlib.pyplot as plt
 
-al_graph_freq = 1 # show accuracy loss graph every n epochs
-wstda_graph_freq = 1 # show weight standard deviation / accuracy graph every n epochs
-
 # Download training data from open datasets.
 training_data = datasets.FashionMNIST(
     root="data",
@@ -115,7 +112,7 @@ def test(dataloader, model):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     return 100*correct, test_loss # accuracy and avg loss
 
-epochs = 50
+epochs = 15
 accuracies, avg_losses, epoch_ind = [], [], [i+1 for i in range(epochs)]
 
 for t in range(epochs):
@@ -126,27 +123,22 @@ for t in range(epochs):
     accuracies.append(accuracy)
     avg_losses.append(avg_loss * 1000)
     epoch_ind_con = epoch_ind[:len(accuracies)]
+    fig, ax = plt.subplots(figsize=(10, 6))
     
-    if (t+1)%al_graph_freq==0:
-        fig, ax = plt.subplots(figsize=(6, 6))
-        
-        ax.plot(epoch_ind_con, accuracies, color='skyblue', linewidth=2, label='Accuracy (%)')
-        ax.plot(epoch_ind_con, avg_losses, color='salmon', linewidth=2, label='Average Loss (magnified by 1000x)')
-        ax.fill_between(epoch_ind_con, accuracies, color='skyblue', alpha=0.3)
-        ax.fill_between(epoch_ind_con, avg_losses, color='salmon', alpha=0.3)
-        
-        plt.style.use('dark_background')
-        
-        plt.title(f'Accuracy and Average Loss: Epochs 1-{t + 1}')
-        plt.xlabel('Epoch')
-        plt.ylabel('Values')
-        
-        plt.grid(True, alpha=0.3)
-        plt.legend()
-        plt.show()
+    ax.plot(epoch_ind_con, accuracies, color='skyblue', linewidth=2, label='Accuracy (%)')
+    ax.plot(epoch_ind_con, avg_losses, color='salmon', linewidth=2, label='Average Loss (magnified by 1000x)')
+    ax.fill_between(epoch_ind_con, accuracies, color='skyblue', alpha=0.3)
+    ax.fill_between(epoch_ind_con, avg_losses, color='salmon', alpha=0.3)
     
-    #if (t+1)%wstda_graph_freq==0:
-    #    fix, ax = plt.subplots(figsize=(10, 6))
+    plt.style.use('dark_background')
+    
+    plt.title('Accuracy and Average Loss Over Epochs')
+    plt.xlabel('Epoch')
+    plt.ylabel('Values')
+    
+    plt.grid(True, alpha=0.3)
+    plt.legend()
+    plt.show()
 
 print("Done!")  
 
